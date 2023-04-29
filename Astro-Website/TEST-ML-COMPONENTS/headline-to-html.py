307cc90fbaabb362a6399e3dd6252df660e3e3a7
Alexandra Savino <avs2167@barnard.edu>
2023-04-29 13:16:19 -0400

    changed headlines-to-html and took header out of ref-sites

--- Astro-Website/TEST-ML-COMPONENTS/headline-to-html.py
+++ Astro-Website/TEST-ML-COMPONENTS/headline-to-html.py
@@ -2,13 +2,13 @@
 #    MIGHT HAVE TO CHANGE TO INCLUDE LINKS?
 
 
-with open('/Users/alexandrasavino/Desktop/Astro-Website/TEST-ML-COMPONENTS/links.txt','r',encoding='utf-8') as link_file:
+with open('Astro-Website/TEST-ML-COMPONENTS/links.txt','r',encoding='utf-8') as link_file:
     link_list = []
     for line in link_file:
         link_list.append(line)
     #print(link_list)
 
-with open('/Users/alexandrasavino/Desktop/Astro-Website/TEST-ML-COMPONENTS/headlines.txt','r',encoding='utf-8') as headline_file:
+with open('Astro-Website/TEST-ML-COMPONENTS/headlines.txt','r',encoding='utf-8') as headline_file:
     headline_list = []
     for line in headline_file:
         line = ' '.join(word.capitalize() for word in line.split())
@@ -29,13 +29,13 @@ for bullet_index in range(len(link_list)):
         + link_list[bullet_index] + '</a></u></li>'
     '''
     list_html += list_item
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/ref-site-page.html','w',encoding='utf-8') as ref_page:
+with open('Astro-Website/PAGES/ref-site-page.html','r',encoding='utf-8') as ref_page:
 #with open('/Users/alexandrasavino/Desktop/Astro Website/PAGES/UR-MOM-TESTING.html','r',encoding='utf-8') as ref_page:
     html_content = ref_page.read()
 
 modified_html_content = html_content.replace("[[[LIST]]]", list_html)
 # THIS HAS TO BE RESET EVERYDAY TO [[[LIST]]] SO THAT THE FILE CAN BE UPDATED!!! 
 
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/ref-site-page.html','w',encoding='utf-8') as file:
+with open('Astro-Website/PAGES/ref-site-page.html','w',encoding='utf-8') as file:
 #with open('/Users/alexandrasavino/Desktop/Astro Website/PAGES/UR-MOM-TESTING.html','w',encoding='utf-8') as file:
     file.write(modified_html_content)
