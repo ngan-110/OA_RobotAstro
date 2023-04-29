1bea757a3a8b0fc9edd2be0fdb10837a586a2b37
Alexandra Savino <avs2167@barnard.edu>
2023-04-29 14:59:57 -0400

    new object html templates

--- Astro-Website/TEST-ML-COMPONENTS/popular-objects-to-html.py
+++ Astro-Website/TEST-ML-COMPONENTS/popular-objects-to-html.py
@@ -39,7 +39,7 @@ object_list = generate_obj_list(popular_topics)
 
 # TODO: Rewrite into functions
 # MODIFYING OBJECT-1-PAGE...
-with open(obj_1_page,'w',encoding='utf-8') as obj1_file:
+with open(obj_1_page,'r',encoding='utf-8') as obj1_file:
     new_content = obj1_file.read()
 modified_object_1_page = new_content.replace(OBJ_1,object_list[0])
 with open(obj_1_page,'w',encoding='utf-8') as file:
@@ -47,7 +47,7 @@ with open(obj_1_page,'w',encoding='utf-8') as file:
 
 
 # MODIFYING OBJECT-2-PAGE...
-with open(obj_2_page,'w',encoding='utf-8') as obj2_file:
+with open(obj_2_page,'r',encoding='utf-8') as obj2_file:
     new_content = obj2_file.read()
 modified_object_2_page = new_content.replace(OBJ_1,object_list[0])
 modified_object_2_page = modified_object_2_page.replace(OBJ_2,object_list[1])
@@ -56,7 +56,7 @@ with open(obj_2_page,'w',encoding='utf-8') as file:
 
 
 # MODIFYING OBJECT-3-PAGE...
-with open(obj_3_page,'w',encoding='utf-8') as obj3_file:
+with open(obj_3_page,'r',encoding='utf-8') as obj3_file:
     new_content = obj3_file.read()
 modified_object_3_page = new_content.replace(OBJ_1,object_list[0])
 modified_object_3_page = modified_object_3_page.replace(OBJ_3,object_list[2])
