a81e528302f1c0714b503061cc45c3e2e997d275
ginnygreen110 <Ginnygreen110@gmail.com>
2023-04-29 13:33:28 -0400

    Update popular-objects-to-html.py

--- Astro-Website/TEST-ML-COMPONENTS/popular-objects-to-html.py
+++ Astro-Website/TEST-ML-COMPONENTS/popular-objects-to-html.py
@@ -1,37 +1,65 @@
-with open('/Users/alexandrasavino/Desktop/Astro-Website/TEST-ML-COMPONENTS/popular_topics.txt','r',encoding='utf-8') as pop_topics_file:
-    object_list = []
-    counter = 0
-    for line in pop_topics_file:
-        line = line[:-1]
-        if counter < 3:
-            object_list.append(line)
-            counter += 1
-        else:
-            break
-#print(object_list)
+popular_topics = 'data/popular_topics.txt'
+obj_1_page = 'Astro-Website\PAGES\object-1-page.html'
+obj_2_page = 'Astro-Website\PAGES\object-2-page.html'
+obj_3_page = 'Astro-Website\PAGES\object-3-page.html'
+OBJ_1 = '[[[OBJECT-1]]]'
+OBJ_2 = '[[[OBJECT-2]]]'
+OBJ_3 = '[[[OBJECT-3]]]'
+
+def generate_obj_list(popular_topics):
+    with open(popular_topics,'r',encoding='utf-8') as pop_topics_file:
+        object_list = []
+        counter = 0
+        for line in pop_topics_file:
+            line = line[:-1]
+            if counter < 3:
+                object_list.append(line)
+                counter += 1
+            else:
+                break
+    return object_list
 
 
+def modify_obj_page(obj_page, OBJ, object_list):
+    with open(obj_page,'r',encoding='utf-8') as obj_page:
+        content = obj_page.read()
+    modified_obj = content.replace(OBJ,object_list[0])
+    if OBJ == OBJ_2:
+        modified_obj = modified_obj.replace(OBJ, object_list[1])
+    if OBJ == OBJ_3:
+        modified_obj = modified_obj.replace(OBJ, object_list[2])
+    with open(obj_page,'w',encoding='utf-8') as file:
+        file.write(modified_obj)
+
+object_list = generate_obj_list(popular_topics)
+modify_obj_page(obj_1_page, OBJ_1, object_list)
+modify_obj_page(obj_2_page, OBJ_2, object_list)
+modify_obj_page(obj_3_page, OBJ_3, object_list)
+
+'''
+# TODO: Rewrite into functions
 # MODIFYING OBJECT-1-PAGE...
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/object-1-page.html','r',encoding='utf-8') as object_1_page:
+with open(obj_1_page,'r',encoding='utf-8') as object_1_page:
     content = object_1_page.read()
 modified_object_1_page = content.replace('[[[OBJECT-1]]]',object_list[0])
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/object-1-page.html','w',encoding='utf-8') as file:
+with open(obj_1_page,'w',encoding='utf-8') as file:
     file.write(modified_object_1_page)
 
 
 # MODIFYING OBJECT-2-PAGE...
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/object-2-page.html','r',encoding='utf-8') as object_2_page:
+with open(obj_2_page,'r',encoding='utf-8') as object_2_page:
     content = object_2_page.read()
 modified_object_2_page = content.replace('[[[OBJECT-1]]]',object_list[0])
 modified_object_2_page = modified_object_2_page.replace('[[[OBJECT-2]]]',object_list[1])
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/object-2-page.html','w',encoding='utf-8') as file:
+with open(obj_2_page,'w',encoding='utf-8') as file:
     file.write(modified_object_2_page)
 
 
 # MODIFYING OBJECT-3-PAGE...
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/object-3-page.html','r',encoding='utf-8') as object_3_page:
+with open(obj_3_page,'r',encoding='utf-8') as object_3_page:
     content = object_3_page.read()
 modified_object_3_page = content.replace('[[[OBJECT-1]]]',object_list[0])
 modified_object_3_page = modified_object_3_page.replace('[[[OBJECT-3]]]',object_list[2])
-with open('/Users/alexandrasavino/Desktop/Astro-Website/PAGES/object-3-page.html','w',encoding='utf-8') as file:
+with open(obj_3_page,'w',encoding='utf-8') as file:
     file.write(modified_object_3_page)
+'''
