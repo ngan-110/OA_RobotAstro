a041946f74310a3716abd4ce29673bf6727c54bc
Alexandra Savino <avs2167@barnard.edu>
2023-04-29 13:00:44 -0400

    abc

---
+++ Astro-Website/TEST-ML-COMPONENTS/IN-SKY-TESTING.py
@@ -0,0 +1,36 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import json
+
+from urllib.request import urlopen
+
+urlopen("https://ipinfo.io/json")
+
+data = json.load(urlopen("https://ipinfo.io/json"))
+
+latitude = float(data['loc'].split(',')[0])
+latitude =  "{:.2f}".format(latitude)
+longitude = float(data['loc'].split(',')[1])
+longitude = "{:.2f}".format(longitude)
+
+location = [latitude, longitude]
+
+
+from object_retrieve import get_object_icrs 
+from object_retrieve import get_time
+
+time = get_time()
+
+#TEST OBJECT
+object = "M3"
+
+RADe_object,in_sky = get_object_icrs(time,location,object)
+
+if in_sky == True:
+    statement = "THIS OBJECT IS UP IN YOUR SKY!"
+else:
+    statement = "THIS OBJECT IS BELOW YOUR HORIZON"
+
+title_string = "LAT: " + latitude + " LONG: " + longitude + " -- " + statement
+
+
