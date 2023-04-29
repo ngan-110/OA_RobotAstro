f5b527a20de64a1d9ac7f72f5b158497fbe5c867
Rhessa <109251845+RhessaWL@users.noreply.github.com>
2023-04-29 15:42:38 -0400

    main.py updates

--- main.py
+++ main.py
@@ -3,25 +3,23 @@
 # import the required modules
 import os
 import argparse
-from website_scrapping import run_sites
-from keyword_processing import run_keywords
-from back_search import run_back_search
+import website_scrapping
+import keyword_processing
+import back_search
 
 # define the main function
 def main():
     # parse command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', required=True)
-    parser.add_argument('--output_dir', required=True)
     args = parser.parse_args()
-
-    run_sites()
-    run_keywords()
-    run_back_search()
     
-    # call the functions from the dependent scripts
-    #my_module1.process_input(args.input_file)
-    #my_module2.save_output(args.output_dir)
+    print(os.path.getsize('data/headlines.txt'))
+    while os.path.getsize('data/headlines.txt') == 0:
+        website_scrapping.run_sites()
+    
+    keyword_processing.run_keywords()
+    back_search.run_back_search()
+
 
 if __name__ == '__main__':
     main()
